fdd0932da59013f2ef2080ddead9adfae56d6e70
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-03-13 22:16:11 +0200

    fixes in files

--- src/SimpleLightBox.js
+++ src/SimpleLightBox.js
@@ -1,11 +1,11 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const lightbox = new SimpleLightbox('.gallery  a', {
-captionsData: 'alt',
-captionPosition: 'bottom',
-captionDelay: 300,
-animationSpeed: 150,
-fadeSpeed: 150,
-close: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 300,
+  animationSpeed: 150,
+  fadeSpeed: 150,
+  close: true,
 });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,50 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 
-import fetchImg  from './fetchImg';
-import { renderImg } from './renderImg';
+import fetchImg from './fetchImg';
 import { lightbox } from './SimpleLightBox';
 
 const refs = {
-  searchForm: document.querySelector('.search-form'),
   input: document.querySelector('input'),
   onBtn: document.querySelector('button'),
   gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
+  loadBtn: document.querySelector('.load-more'),
 };
 
-refs.loadMoreBtn.classList.add('is-hidden');
+refs.loadBtn.classList.add('is-hidden');
 refs.onBtn.addEventListener('click', onSubmit);
-refs.loadMoreBtn.addEventListener('click', loadMore);
+refs.loadBtn.addEventListener('click', loadMore);
 
 let page = 1;
+
+const markup = data => {
+  const galleryElements = data.hits
+    .map(
+      img =>
+        `<a class="gallery link" href="${img.largeImageURL}"><div class="photo-card">
+  <img class="image" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${img.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${img.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${img.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${img.downloads}
+    </p>
+  </div>
+</div></a>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', galleryElements);
+  refs.loadBtn.classList.remove('is-hidden');
+
+  lightbox.refresh();
+};
 
 async function onSubmit(evt) {
   evt.preventDefault();
